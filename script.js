2c0ddb23075f36660f22a469cfef0fdeab87fb46
Ederaldo Souza <84224107+EderSouza93@users.noreply.github.com>
2024-06-23 14:11:10 -0300

    Fix bugs geolocation and send data to db

--- script.js
+++ script.js
@@ -9,58 +9,74 @@ const submitForm = () => {
   const checkType = document.getElementById("checkType").value;
   const dateTime = new Date().toLocaleString();
 
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(position => {
-
-          const latitude = position.coords.latitude;
-          const longitude = position.coords.longitude;
-
-          const data = {
-            data: [
-              {
-                name: name,
-                tel: tel,
-                gerente: geren,
-                checkType: checkType,
-                dateTime: dateTime,
-                latitude: latitude,
-                longitude: longitude,
-              },
-            ],
-          };
-
-          fetch(SHEETDB_API_URL, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${BEARER_TOKEN}`,
-            },
-            body: JSON.stringify(data),
-          })
-            .then((response) => response.json())
-            .then((data) => {
-              console.log(data);
-              alert('Formulário enviado com sucesso!')
-            })
-            .catch((error) => {
-              console.error("Erro:", error);
-              alert('Erro ao enviar o formulário.')
-            });
-
-        },
-        (error) => {
-          console.error(error);
-          if (error.code === error.PERMISSION_DENIED) {
-        alert('Localização é necessária para enviar o formulário.');
-        location.reload();
-          } else {
-          alert("Não foi possível obter a localização.");
-        }
-  });
-
-  } else {
-    alert("Geolocalização não é suportada pelo seu navegador.");
+  if(!name || !tel || !geren || !checkType) {
+    alert('Por favor, preencha todos os campos obrigatórios.');
+    return
   }
+
+  //Verifica a permissão de geolocalização
+  if(navigator.geolocation){
+  navigator.permissions.query({ name: "geolocation" }).then((PermissionStatus) => {
+      if (PermissionStatus.state === "granted" || PermissionStatus.state === "prompt") {
+        //Solicita a localização
+        navigator.geolocation.getCurrentPosition(position => {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+            submitData(name, tel, geren, checkType, dateTime, latitude, longitude);
+          }, error => {
+            handleGeolocationError(error);
+          });
+      } else {
+        //Se a permissão foi negada, informa o usuário e recarrega a página
+        alert("Localização é necessária para enviar o formulário.");
+        location.reload();
+      }
+    });
+} else {
+  alert('Geolocalização não é suportada pelo seu navegador.');
+}
+}
+
+const submitData = (name, tel, geren, checkType, dateTime, latitude, longitude) => {
+  const data = {
+    "data": [
+      {
+        "name": name,
+        "tel": tel,
+        "gerente": geren,
+        "checkType": checkType,
+        "dateTime": dateTime,
+        "latitude": latitude,
+        "longitude": longitude,
+      },
+    ],
+  };
+
+  fetch(SHEETDB_API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${BEARER_TOKEN}`,
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      alert("Formulário enviado com sucesso!");
+    })
+    .catch((error) => {
+      console.error("Erro:", error);
+      alert("Erro ao enviar o formulário.");
+    });
 };
 
-
+const handleGeolocationError = (error) => {
+  console.error(error);
+  if(error.code === error.PERMISSION.DENIED) {
+    alert('Necessário permitir a localização para o envio do formulário.');
+    location.reload();
+  } else {
+    alert('Não foi possivel obter a localização.')
+  }
+}
